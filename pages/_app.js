92a75c45e864613b6441c23363007d3118250b20
AbiNabat <154244797+AbiNabat@users.noreply.github.com>
2024-02-03 19:56:27 +0300

    redux -reducers

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.scss'
+import {Provider} from "react-redux";
+import store from "../store";
+import {PersistGate} from "redux-persist/integration/react";
+import {persistStore} from "redux-persist";
 
+let persistor = persistStore(store);
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return<Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
+    <Component {...pageProps} />
+    </PersistGate>
+  </Provider>
+  
 }
 
 export default MyApp
